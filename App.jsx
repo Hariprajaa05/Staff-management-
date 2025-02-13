a2110eed39cdb114825469ad64c9434d66854511
Hariprajaa B <953622104030@ritrjpm.ac.in>
2025-02-13 16:02:37 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,16 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./login";
 import Dashboard from "./Dashboard";
 import Report from "./Report"; // Import the new Report component
-import JApage from "./japage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/report" element={<Report/>} /> {/* Added Report Route */}
-        <Route path="/japage" element={<JApage/>}/>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/report" element={<Report />} /> {/* Added Report Route */}
       </Routes>
     </Router>
   );
